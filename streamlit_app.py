1cc16d122d816381beb033973b17b2549daf1505
Christophe Cestonaro <47605907+C3st0@users.noreply.github.com>
2024-12-03 13:58:59 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,16 @@
 import streamlit as st
+import numpy as np
+import pandas as pd
+
+df = pd.DataFrame(
+    np.random.randn(10, 3),
+    columns=['a', 'b', 'c']
+)
+df["pos"] = df["a"]>0 
 
 st.title("🎈 My new app")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+
+edited_df = st.data_editor(df)
